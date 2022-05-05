a09f217595387de2ede9fe1ccea0daa81b2ef372
Alexssandro Da Silva Vidal <101327885+AlexdaSilvaVidal@users.noreply.github.com>
2022-05-05 10:12:01 -0300

    Add files via upload

---
+++ exe19_aluno2.php
@@ -0,0 +1,43 @@
+<?php
+ function validaMat(){
+  while (!feof($arquivoAlunoin)){
+	 echo fgets ($aquivoAluno);
+    }
+ }
+
+ if($_SERVER ["REQUEST_METHOD"] == "POST"){
+   $matricula = $_POST ["matricula"];
+   $nome = $_POST ["nome"];
+   $email = $_POST ["email"];
+   
+   $arquivoAluno = fopen ("Alunos.txt", "w") or die ("arquivo com problema"); 
+   while (!feof($arquivoAlunoin)){
+	 echo fgets ($aquivoAluno);
+   }
+   $cabecalho = "matricula;nome;email \n";
+   $arquivoAlunow = fopen ("Alunos.txt", "w") or die ("arquivo com problema"); 
+   fwrite ($arquivoAlunow, $cabecalho);
+   $linha = $matricula . ";" . $nome . ";" . $email;
+   fwrite ($arquivoAlunow, $linha);
+   fclose($arquivoAlunow);
+ }
+?>
+
+<html>
+<head>
+</head>
+<body>
+<h1> Incluir Aluno </h1>  
+<br>
+   <form action="exe19_aluno.php" method="POST">
+
+     Nome: <input type="text" name="nome">
+     <br>
+     Matricula: <input type="text" name="matricula">
+     <br>
+     Email: <input type="text" name="email">
+     <br>
+	 <input type="submit" value="Enviar">
+   </form>
+</body>
+</html>
