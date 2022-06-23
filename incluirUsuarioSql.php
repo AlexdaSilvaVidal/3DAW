a647496847038b1761918f04e6b21e0a477102df
Alexssandro Da Silva Vidal <101327885+AlexdaSilvaVidal@users.noreply.github.com>
2022-06-23 10:59:47 -0300

    Add files via upload

---
+++ incluirUsuarioSql.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<script>
+    function enviaForm() {
+        let mat = document.getElementById("matricula").value;
+        console.log("mat: " + mat);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            console.log("this.readyState: " + this.readyState);
+            if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+                document.getElementById("resultado").innerHTML = xhttp.responseText;
+            }
+        };
+        //console.log("this.readyState: " + this.readyState);
+        xhttp.open("GET", "incluirUsuarioSql.php", true);
+        xhttp.send();
+    }
+</script>
+</head>
+<body>
+<h1>Incluir Usuario</h1><br><br>
+<div>
+<a href="incluirUsuarioSql.html">Incluir Usuário</a><br>
+<a href="alterarUsuarioSql.html">Alterar Usuário</a><br>
+<a href="excluirUsuarioSql.html">Excluir Usuário</a><br>
+<a href="listarUsuarioSql.html">Listar Usuário</a><br>
+<a href="listarTodosUsuarioSql.html">Listar Todos Usuários</a><br>
+</div>
+
+<form action="incluirUsuarioSql.php" method="POST">
+    Matricula: <input type=text name="matricula" id="matricula" value=''> <br>
+    nome: <input type=text name="nome" value=''> <br>
+    função: <input type=text name="funcao"> <br>
+	senha: <input type=text name="senha"> <br>
+    <br><br>
+    <input type="button" value="Incluir" onClick="enviaForm();">
+</form>
+<div id="resultado"></div>
+<br>
+</body>
+</html>
